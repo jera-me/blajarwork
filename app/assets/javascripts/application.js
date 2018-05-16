d3e3fd9a8af3c2a582cd8f44f58903791c36653e
Jerami <hi@jera.me>
2018-05-16 05:11:41 +0000

    add controller to stories view index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,6 @@
 class StoriesController < ApplicationController
   def index
+    @stories = Story.all
+    @speaker = Speaker.all
   end
 end
